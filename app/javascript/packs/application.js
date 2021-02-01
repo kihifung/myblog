26d1e4357a94d6c3aaef3d02af67befb749bc7ff
Annie <annielin28815@gmail.com>
2021-02-02 02:59:40 +0800

    add homepage and jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,22 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// import '../cch'
+import 'jquery'
+import $ from 'jquery'
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20) 
+      $(".navbar").addClass("sticky");
+    else
+      $(".navbar").removeClass("sticky");
+  });
 
+  $('.menu-toggler').click(function(){
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root "blogs#index"
+  root "pages#index"
   resources :blogs
+  resources :pages
 end
