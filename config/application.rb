2847ec4b306822653239210065101466c75fffa2
kihifung <kihifung@gmail.com>
2021-02-02 16:15:51 +0800

    add config.assets

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module MyBlog
     config.hosts << "www.annielin.cc"
     config.hosts << "annielin.cc"
     config.hosts << "annie-cc.herokuapp.com"
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
